8077a840731aa82f8f81f04981ac4b1825a40d92
Aashish Chakravarty <chakravartyaashish@gmail.com>
2024-06-28 18:08:11 +0530

    expose variables in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
  */
 const nextConfig = {
   output: "export",
+  env: {
+    SERVICE_ID: process.env.SERVICE_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    PUBLIC_KEY: process.env.PUBLIC_KEY,
+  },
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
