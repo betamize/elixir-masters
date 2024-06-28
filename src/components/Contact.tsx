9f64b2de4672fcaac90a815a540930b4cad44b40
Aashish Chakravarty <chakravartyaashish@gmail.com>
2024-06-28 17:02:36 +0530

    Configure emailjs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,41 +1,41 @@
-'use client';
-import React from 'react';
-// import { useForm, SubmitHandler } from 'react-hook-form';
-import { SectionTitle } from './SectionTitle';
-import { useForm, useWatch } from 'react-hook-form';
+"use client";
+import React, { useState } from "react";
+import { SectionTitle } from "./SectionTitle";
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
-interface IFormInput {
-  name: string;
-  email: string;
-  subject: string;
-  message: string;
-}
-
-interface PopupWidgetProps {
-  preTitle?: string;
-  title?: string;
-  align?: 'left' | 'center';
-  children?: React.ReactNode;
-}
-
-// export function Contact(props: PopupWidgetProps) {
 export const Contact = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful },
     reset,
-  } = useForm({ mode: 'onTouched' });
+  } = useForm({ mode: "onTouched" });
+
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const onSubmit = async (data: any, e: any) => {
-    console.log(data);
-    reset();
-  };
+    e.preventDefault();
 
-  // const onSubmit = (data: any) => {
-  //   console.log(data);
-  //   reset();
-  // };
+    await emailjs
+      .send(
+        process.env.SERVICE_ID as string,
+        process.env.TEMPLATE_ID as string,
+        data,
+        {
+          publicKey: process.env.PUBLIC_KEY as string,
+        }
+      )
+      .then(
+        () => {
+          setIsSuccess(true);
+          reset();
+        },
+        (error) => {
+          setIsSuccess(false);
+        }
+      );
+  };
 
   return (
     <section id="contact">
@@ -45,80 +45,6 @@ export const Contact = () => {
       />
 
       <div className="w-full max-w-2xl mx-auto pb-8 pt-4 md:pb-12 md:pt-6">
-        {/* <form action="#" method="POST">
-          <div className="mb-6">
-            <label
-              htmlFor="name"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              placeholder="Your Name"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              placeholder="Your Email"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="subject"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              name="subject"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              placeholder="Subject"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              rows={6}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              placeholder="Your Message"
-              required
-            ></textarea>
-          </div>
-          <div className="text-center">
-            <button
-              type="submit"
-              className="bg-indigo-600 text-white px-6 py-3 rounded-full hover:bg-indigo-700 transition-colors duration-300"
-            >
-              Send Message
-            </button>
-          </div>
-        </form> */}
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-6">
             <label
@@ -131,10 +57,10 @@ export const Contact = () => {
               type="text"
               id="name"
               className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 ${
-                errors.name ? 'border-red-500' : ''
+                errors.name ? "border-red-500" : ""
               }`}
               placeholder="Your Name"
-              {...register('name', { required: 'Name is required' })}
+              {...register("name", { required: "Name is required" })}
             />
             {errors.name && (
               <p className="text-red-500 text-sm">
@@ -153,14 +79,14 @@ export const Contact = () => {
               type="email"
               id="email"
               className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 ${
-                errors.email ? 'border-red-500' : ''
+                errors.email ? "border-red-500" : ""
               }`}
               placeholder="Your Email"
-              {...register('email', {
-                required: 'Email is required',
+              {...register("email", {
+                required: "Email is required",
                 pattern: {
                   value: /\S+@\S+\.\S+/,
-                  message: 'Email is invalid',
+                  message: "Email is invalid",
                 },
               })}
             />
@@ -181,10 +107,10 @@ export const Contact = () => {
               type="text"
               id="subject"
               className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 ${
-                errors.subject ? 'border-red-500' : ''
+                errors.subject ? "border-red-500" : ""
               }`}
               placeholder="Subject"
-              {...register('subject', { required: 'Subject is required' })}
+              {...register("subject", { required: "Subject is required" })}
             />
             {errors.subject && (
               <p className="text-red-500 text-sm">
@@ -203,10 +129,10 @@ export const Contact = () => {
               id="message"
               rows={6}
               className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 ${
-                errors.message ? 'border-red-500' : ''
+                errors.message ? "border-red-500" : ""
               }`}
               placeholder="Your Message"
-              {...register('message', { required: 'Message is required' })}
+              {...register("message", { required: "Message is required" })}
             ></textarea>
             {errors.message && (
               <p className="text-red-500 text-sm">
@@ -223,6 +149,20 @@ export const Contact = () => {
             </button>
           </div>
         </form>
+
+        {isSubmitSuccessful && (
+          <div className="flex flex-col items-center justify-center h-full text-center text-white rounded-md">
+            {isSuccess ? (
+              <h3 className="py-5 text-xl text-green-500">
+                Message sent successfully
+              </h3>
+            ) : (
+              <h3 className="text-xl text-red-400 py-7">
+                Oops, Something went wrong!
+              </h3>
+            )}
+          </div>
+        )}
       </div>
     </section>
   );
